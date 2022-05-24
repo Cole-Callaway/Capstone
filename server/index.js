c7b8140774ba578d8171723e502f706ce8ea78aa
Cole Callaway <nccallaway54@gmail.com>
2022-05-23 19:38:31 -0600

    server setup

--- server/index.js
+++ server/index.js
@@ -7,4 +7,16 @@ const { SERVER_PORT } = process.env;
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/js", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/script.js"));
+});
+
+app.get("/styles", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/styles.css"));
+});
+
 app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`));
